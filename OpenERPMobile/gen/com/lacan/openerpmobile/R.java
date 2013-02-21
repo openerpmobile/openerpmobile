5f13d6ff78bf867c2f05fa12e0358d4af127bf8b
Pawel <PawelKobojek@gmail.com>
2013-02-21 16:33:33 +0100

    <21.02.13>, <16:34> Poprawiony przycisk

--- OpenERPMobile/gen/com/lacan/openerpmobile/R.java
+++ OpenERPMobile/gen/com/lacan/openerpmobile/R.java
@@ -26,7 +26,7 @@ public final class R {
     public static final class drawable {
         public static final int background=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int login=0x7f020002;
+        public static final int login_button=0x7f020002;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f090000;
