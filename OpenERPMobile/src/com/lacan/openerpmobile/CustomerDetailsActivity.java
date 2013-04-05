29f1667fab45d7e385d553b9817b4541b160812b
Pawel <PawelKobojek@gmail.com>
2013-04-05 22:24:06 +0200

    22:23 Customer Details Activity (jeszcze nie wszystko)

--- OpenERPMobile/src/com/lacan/openerpmobile/CustomerDetailsActivity.java
+++ OpenERPMobile/src/com/lacan/openerpmobile/CustomerDetailsActivity.java
@@ -3,14 +3,25 @@ package com.lacan.openerpmobile;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
 
 public class CustomerDetailsActivity extends Activity
 {
+	TextView textViewName;
 	TextView textViewPhone;
+	TextView textViewMail;
+	TextView textViewWeb;
+	TextView textViewAddress;
+
+	String name;
 	String phone;
+	String email;
+	String web;
+	String address;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -24,10 +35,13 @@ public class CustomerDetailsActivity extends Activity
 		Cursor cursor = ((OpenERPApp) getApplication()).customerData.query(id
 				.toString());
 
-		int ind = cursor.getColumnIndex(CustomerData.C_PHONE);
-
 		cursor.moveToFirst();
-		phone = cursor.getString(ind);
+		name = cursor.getString(cursor.getColumnIndex(CustomerData.C_NAME));
+		phone = cursor.getString(cursor.getColumnIndex(CustomerData.C_PHONE));
+		email = cursor.getString(cursor.getColumnIndex(CustomerData.C_EMAIL));
+		web = cursor.getString(cursor.getColumnIndex(CustomerData.C_WEBISTE));
+		address = cursor.getString(cursor
+				.getColumnIndex(CustomerData.C_ADDRESS));
 
 		setContentView(R.layout.activity_customer_details);
 	}
@@ -35,8 +49,21 @@ public class CustomerDetailsActivity extends Activity
 	@Override
 	protected void onResume()
 	{
-		textViewPhone = (TextView) findViewById(R.id.phone_number);
+		textViewName = (TextView) findViewById(R.id.textName);
+		textViewName.setText(name);
+
+		textViewPhone = (TextView) findViewById(R.id.textPhone);
 		textViewPhone.setText(phone);
+
+		textViewMail = (TextView) findViewById(R.id.textMail);
+		textViewMail.setText(email);
+
+		textViewWeb = (TextView) findViewById(R.id.textWeb);
+		textViewWeb.setText(web);
+
+		textViewAddress = (TextView) findViewById(R.id.textAdress);
+		textViewAddress.setText(address);
+
 		super.onResume();
 	}
 
@@ -46,6 +73,24 @@ public class CustomerDetailsActivity extends Activity
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.customer_details, menu);
 		return true;
+	}
+
+	public void onPhoneButtonClick(View view)
+	{
+		Intent intent = new Intent(Intent.ACTION_CALL);
+
+		intent.setData(Uri.parse("tel:" + phone));
+		startActivity(intent);
+	}
+
+	public void onMailButtonClick(View view)
+	{
+
+	}
+
+	public void onWebButtonClick(View view)
+	{
+
 	}
 
 }
